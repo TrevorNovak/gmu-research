08be64f3a59cba91ce3e945b18f594861d9bffef
Trevor Novak <trevornovak.developer@gmail.com>
2018-02-12 11:03:25 -0500

    Added some basic comments to process.py

--- python-scripts/log_analyzer/processor.py
+++ python-scripts/log_analyzer/processor.py
@@ -30,8 +30,13 @@ class Processor:
         self.encoded_matrix = [ ]
 
     def process(self, token_collection, outfile, infile, flag1):
+        """
+        Main processing function for entire analyzer. Orchaestrates smaller
+        processing functions such as csv_process() and matrix_process().
+        """
         flag2 = 1
         token_collection_list = []
+        self.read_config_file("config.txt")
 
         for token in token_collection:
             token_collection_list.append(token)
@@ -44,27 +49,11 @@ class Processor:
             self.csv_process(token_collection_list, outfile, flag1)
             self.matrix_process(token_collection_list)
 
-    def build_line_map(self, infile):
-        temp_key = [ ]
-
-        with open(infile, "r") as csvfile:
-            linereader = csv.reader(csvfile, delimiter=',')
-            for line in linereader:
-                temp_key.append(line[1])                # Get "From" bus
-                temp_key.append(line[2])                # Get "To" bus
-                key = tuple(temp_key)                   # Use "From" and "To" as hashmap key
-                value = line[0]                         # Use line num as value in hashmap
-                # if key in self.line_map:
-                #     print("Current Line: " + str(value) +  " Line: " + str(self.line_map[key])+ " Key: " + str(key))
-
-                self.line_map[key] = value
-                temp_key = []
-
-    def print_line_map(self):
-        for entry in self.line_map:
-            print(str(entry) + " = " + str(self.line_map[entry]))
-
     def csv_process(self, token_collection, filename, flag1):
+        """
+        Main processing function for generating CSV output. Orchaestrates smaller
+        processing functions for each log section (currently five + header)
+        """
         current_replication = 0
 
         for token in token_collection:
@@ -105,6 +94,10 @@ class Processor:
         self.write_to_csv(filename)
 
     def csv_process_header(self, token, current_replication):
+        """
+        Function used to determine how to process header tokens for csv file
+        output.
+        """
         self.isReinforced = -1
         self.current_section = self.get_section(token)
         if self.current_section == 5:
@@ -112,6 +105,11 @@ class Processor:
                 self.store_and_reset(current_replication)
 
     def csv_process_section_one(self, token):
+        """
+        Function used to determine how to process section one data tokens for
+        csv file output. Section one tells us which lines are reinforced and
+        which lines were inititally tripped.
+        """
         if self.current_token_number > self.TOTAL_COL:
             self.current_token_number = 1
         if self.current_token_number == 1:               # LineNum token
@@ -128,12 +126,23 @@ class Processor:
             self.current_token_number += 1               # No match
 
     def csv_process_section_two(self, token):
+        """
+        Function used to determine how to process section two data tokens for
+        csv file output. Section two tells us which lines are tripped after
+        cascading power failure is complete. For csv output, we simply need to
+        know how many.
+        """
         next_line = int(token.line)
         if next_line > self.curr_line:
             self.curr_line = next_line
             self.total_tripped += 1
 
     def csv_process_section_three(self, token):
+        """
+        Function used to determine how to process section three data tokens for
+        csv file output. Section three tell us shedding load for a given bus. We
+        use this information to calculate total load shedding.
+        """
         if self.current_token_number > 4:
             self.current_token_number = 1
         if self.current_token_number == 4:
@@ -141,15 +150,29 @@ class Processor:
         self.current_token_number += 1
 
     def csv_process_section_four(self):
+        """
+        Function used to determine how to process section four data tokens for
+        csv file output. Section four tells us which generators tripped. For csv
+        processing, we simply want to know how many.
+        """
         self.total_tripped_generators += 1
 
     def csv_process_section_five(self, token):
+        """
+        Function used to determine how to process section five data tokens for
+        csv file output. Section five tells us the replication index.
+        """
         current_replication = int(token.value)
         if current_replication > self.total_replications:
             self.total_replications = current_replication
         return current_replication
 
     def matrix_process(self,token_collection):
+        """
+        Main processing function for generating sparse matrix output.
+        Orchaestrates smaller processing functions for each log section (currently five + header).
+        Currently, code is not broken up into smaller functions. That is on todo list.
+        """
         #numlines = len(self.line_map)
         self.build_matrix_map()
         numlines = 233
@@ -235,6 +258,11 @@ class Processor:
         # dmatrix.tofile(outfile, sep=" ", format="%s")
 
     def build_matrix_map(self):
+        """
+        Helper function used to build up the matrix map. Given a particular
+        hardening plan, and a replication count, it will map this key to a row
+        number in the matrix.
+        """
         #((1, 3), 1) : 1
         total_count = 0
         temp_count = 1
@@ -276,6 +304,10 @@ class Processor:
         self.encoded_matrix = [[0 for x in range(col)] for y in range(row)]
 
     def store_and_reset(self, replication):
+        """
+        Helper function used to store a hardening plan into the dictionary and
+        reset all values required for the new processing cycle.
+        """
         plan_key = self.add_plan(self.plans, replication)
         if self.plan_length < len(plan_key):
             self.plan_length = len(plan_key)
@@ -283,6 +315,9 @@ class Processor:
         self.reset()
 
     def reset(self):
+        """
+        Resets valus to their default states.
+        """
         self.total_initial_tripped = 0
         self.total_tripped = 0
         self.total_tripped_generators = 0
@@ -293,6 +328,10 @@ class Processor:
         self.plans = [ ]
 
     def add_to_csvrows(self, replication_index):
+        """
+        Builds a list of csv rows so that we only need to do one set of writes
+        at the end of the csv processing cycle.
+        """
         row = []
         for hline in self.plans:
             row.append(hline)
@@ -306,6 +345,10 @@ class Processor:
         self.csv_rows.append(row)
 
     def write_to_csv(self, filename):
+        """
+        Writes the csv header and csv_rows list to a csv file. This is the final
+        output.
+        """
         with open(filename, 'w') as csvfile:
             filewriter = csv.writer(csvfile)
             column_header = self.build_csv_header()
@@ -314,6 +357,9 @@ class Processor:
                 filewriter.writerow(row)
 
     def build_csv_header(self):
+        """
+        Builds the csv_header based on output requirements.
+        """
         col_list = []
         for i in range(self.plan_length):
             col_list.append('LineNum')
@@ -327,14 +373,53 @@ class Processor:
         return col_list
 
     def add_plan(self, plan, replication):
+        """
+        Deprecated. Convert plan into a tuple and store it in the dictionary along with its
+        replication index.
+        """
         plan_key = tuple(plan)
         self.hardening_plans[plan_key] = replication
         return plan_key
 
     def get_section(self, token):
+        """
+        Get section header string from the section_map in strings.py. Used for
+        processing header tokens.
+        """
         myregex = '=+'
         line = str(token.value)
         section_text = re.sub(myregex, '', line)
         section_text = section_text.strip()
         #print(section_text)
         return section_table[section_text]
+
+    def build_line_map(self, infile):
+        """
+        Deprecated. Reads in a line map from a given file and builds a line mapping
+        of power lines using the To and From buses as keys into the hashmap.
+        """
+        temp_key = [ ]
+
+        with open(infile, "r") as csvfile:
+            linereader = csv.reader(csvfile, delimiter=',')
+            for line in linereader:
+                temp_key.append(line[1])                # Get "From" bus
+                temp_key.append(line[2])                # Get "To" bus
+                key = tuple(temp_key)                   # Use "From" and "To" as hashmap key
+                value = line[0]                         # Use line num as value in hashmap
+                # if key in self.line_map:
+                #     print("Current Line: " + str(value) +  " Line: " + str(self.line_map[key])+ " Key: " + str(key))
+
+                self.line_map[key] = value
+                temp_key = []
+
+    def print_line_map(self):
+        for entry in self.line_map:
+            print(str(entry) + " = " + str(self.line_map[entry]))
+
+    def read_config_file(self, conf):
+        pattern = r"[a-zA-Z0-9 _-]+="
+
+        with open(conf, 'r') as config:
+            values = config.read()
+        return values
