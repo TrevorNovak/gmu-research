22ee3ee53ef3d9dcbc43931bff0d5247945dad98
Trevor Novak <trevornovak.developer@gmail.com>
2018-02-14 10:33:30 -0500

    Initital testing framework created.

--- python-scripts/log_analyzer/tests.py
+++ python-scripts/log_analyzer/tests.py
@@ -1,12 +1,19 @@
 import unittest
 from processor import *
 
-class TestAnalyzerMethods(unittest.TestCase):
+class TestProcessorMethods(unittest.TestCase):
 
-    def test_build_matrix(self):
+    def test_build_matrix01(self):
         p = Processor()
         p.build_matrix(3, 3)
         self.assertEqual(p.encoded_matrix, [[0,0,0],[0,0,0],[0,0,0]])
+
+    def test_build_matrix02(self):
+        p = Processor()
+        p.build_matrix(3, 4)
+        self.assertNotEqual(p.encoded_matrix, [[0,0,0],[0,0,0],[0,0,0]])
+
+
 
 if __name__ == '__main__':
     unittest.main()
